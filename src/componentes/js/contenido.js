e5db27cee6f3537fac07f6d4f0cbbd39f218647c
Gustavo Dextre <gustavodextrezubieta@gmail.com>
2021-02-20 14:19:42 -0500

    3rd change

--- src/componentes/js/contenido.js
+++ src/componentes/js/contenido.js
@@ -21,20 +21,38 @@ function ContenidoPrincipal(){
       </ScrollPage>
 
       <ScrollPage page={1}>
-        <Animator animation={ZoomInScrollOut}>
-          <div className="mt-4 px-5">
-            <div className="container-fluid fondoprincipal2 text-white py-3 text-center rounded">
-              <p>Buscamos generar una experiencia diferente al momento de aprender, tanto para estudiantes y para los profesores</p>
-              <p>Con HoloEdu AR los estudiante podrán tener a su profesor como si estuviera frente a ellos acompañados de una experiencia interactiva de contenidos en realidad aumentada</p>
-              <p>Nuestra solución permitirá dar un siguiente paso para revolucionar la educación</p>
-            </div>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200),ZoomIn())}>
+        <div className="mt-4 px-2">
+          <div className="container-fluid fondoprincipal2 text-white py-3 text-center rounded">
+          <p>Buscamos generar una experiencia diferente al momento de aprender, tanto para estudiantes y para los profesores</p>
           </div>
+        </div>
         </Animator>
       </ScrollPage>
 
       <ScrollPage page={2}>
         <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200),ZoomIn())}>
-        <div className="mt-4 px-5">
+        <div className="mt-4 px-2">
+          <div className="container-fluid fondoprincipal2 text-white py-3 text-center rounded">
+          <p>Con HoloEdu AR los estudiante podrán tener a su profesor como si estuviera frente a ellos acompañados de una experiencia interactiva de contenidos en realidad aumentada</p>
+          </div>
+        </div>
+        </Animator>
+      </ScrollPage>
+
+      <ScrollPage page={3}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200),ZoomIn())}>
+        <div className="mt-4 px-2">
+          <div className="container-fluid fondoprincipal2 text-white py-3 text-center rounded">
+            <p>Nuestra solución permitirá dar un siguiente paso para revolucionar la educación</p>
+          </div>
+        </div>
+        </Animator>
+      </ScrollPage>
+
+      <ScrollPage page={4}>
+        <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200),ZoomIn())}>
+        <div className="mt-4 px-2">
           <div className="container-fluid fondoprincipal2 text-white py-3 text-center rounded">
             <h3>Descripción</h3>
                 <p>Es un app de realidad aumentada que mediante un holograma de una persona, permitirá que los alumnos puedan ver a su profesor en el mismo entorno que su presentación.</p>
@@ -43,29 +61,29 @@ function ContenidoPrincipal(){
         </Animator>
       </ScrollPage>
 
-      <ScrollPage page={3}>
+      <ScrollPage page={5}>
       <Animator animation={batch(Fade())}>
-          <div className="mt-4 px-5">
+          <div className="mt-4 px-2">
             <div className="container-fluid fondoprincipal2 text-white py-3 text-center rounded">
               <h3>Pitch elevator</h3>
-              <iframe width="717" height="538" src="https://www.youtube.com/embed/BZZ3whdJANE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <iframe className="cuadro" src="https://www.youtube.com/embed/BZZ3whdJANE" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
           </div>
       </Animator>
       </ScrollPage>
 
-      <ScrollPage page={4}>
+      <ScrollPage page={6}>
       <Animator animation={batch(Fade())}>
-        <div className="mt-4 px-5">
+        <div className="mt-4 px-2">
           <div className="container-fluid fondoprincipal2 text-white py-3 text-center rounded">
             <h3>Video Demo</h3>
-            <iframe width="717" height="538" src="https://www.youtube.com/embed/BZZ3whdJANE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe className="cuadro" src="https://www.youtube.com/embed/BZZ3whdJANE" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
         </div>
       </Animator>
       </ScrollPage>
 
-      <ScrollPage page={5}>
+      <ScrollPage page={7}>
       <Animator animation={batch(Fade(), Sticky(), MoveOut(0, -200),ZoomIn())}>
           <div className="m-0 container-fluid">
             <div className="row pt-5 px-5">
